807d0852f7db871e74046507fff5831fc49f0af1
Alex Schittko <alex4108@live.com>
2021-02-22 04:14:10 -0600

    Move nginx configurations to container during build

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,3 +3,6 @@ set -x
 
 docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm/v7 -t alex4108/scoreboard-frontend . --push
+
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm/v7 -f Dockerfile.tls -t alex4108/scoreboard-frontend-tls . --push
