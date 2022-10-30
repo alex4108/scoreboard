9e7df64ebeb0afd7d03d0429f01457b7d1edf09c
Alex Schittko <alex4108@live.com>
2022-10-30 00:48:09 -0600

    Support GIT_SHA on container image

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
+# Get long git commit sha
+GIT_SHA=$(git rev-parse HEAD)
+
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm/v7 -t alex4108/scoreboard-backend . --push
+docker buildx build --platform linux/amd64,linux/arm/v7 -t alex4108/scoreboard-backend:${GIT_SHA} . --push
 
--- frontend/build.sh
+++ frontend/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm/v7 -t alex4108/scoreboard-frontend . --push
+# Get long git commit sha
+GIT_SHA=$(git rev-parse HEAD)
 
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm/v7 -f Dockerfile.tls -t alex4108/scoreboard-frontend-tls . --push
+docker buildx build --platform linux/amd64,linux/arm/v7 -t alex4108/scoreboard-frontend:${GIT_SHA} . --push
+
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm/v7 -f Dockerfile.tls -t alex4108/scoreboard-frontend-tls:${GIT_SHA} . --push
